f682b4c5d96e11e1319cfc2e9b967bc072ee076a
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-09-30 15:29:32 +0300

    feat: add Opera-specific locale validator and update locale builder logic

---
+++ src/cli/builders/locale/OperaLocaleValidator.ts
@@ -0,0 +1,15 @@
+import {Language, LocaleValidator as LocaleValidatorContract} from "@typing/locale";
+import {Browser} from "@typing/browser";
+
+import LocaleValidator from "./LocaleValidator";
+
+export default class extends LocaleValidator implements LocaleValidatorContract {
+
+    protected get nameLimit(): number {
+        return 75;
+    }
+
+    constructor(browser: Browser, language: Language) {
+        super(browser, language);
+    }
+}
--- src/cli/builders/locale/index.ts
+++ src/cli/builders/locale/index.ts
@@ -1,17 +1,25 @@
 import LocaleBuilder from "./LocaleBuilder";
 import LocaleValidator from "./LocaleValidator";
+import OperaLocaleValidator from "./OperaLocaleValidator";
 
 import {extractLocaleKey} from "@locale/utils";
 
 import {Language, LocaleBuilder as LocaleBuilderContract} from "@typing/locale";
 import {ReadonlyConfig} from "@typing/config";
+import {Browser} from "@typing/browser";
 
 export {LocaleBuilder, LocaleValidator};
 
 export default (language: Language, config: ReadonlyConfig): LocaleBuilderContract => {
-    const {browser, name, shortName, description} = config;
+    const {browser, name, shortName, description, lang} = config;
 
-    const validator = new LocaleValidator(browser, language)
+    let Validator = LocaleValidator;
+
+    if (browser === Browser.Opera && lang !== language) {
+        Validator = OperaLocaleValidator;
+    }
+
+    const validator = new Validator(browser, language)
         .setNameKey(extractLocaleKey(name))
         .setShortNameKey(extractLocaleKey(shortName))
         .setDescriptionKey(extractLocaleKey(description));
